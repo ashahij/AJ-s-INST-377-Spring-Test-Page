fd629dadaf8c601daeb6fdc0efa9ef5fed87b749
ashahij <56201025+ashahij@users.noreply.github.com>
2020-11-06 13:34:07 -0500

    Lab 7 Update

--- Lab_7/js/setSessionData.js
+++ Lab_7/js/setSessionData.js
@@ -4,33 +4,35 @@ function getBusRoute() {
   let busRoute = document.getElementById("busroute").value; // Your code here
 
   if ((typeof busRoute !== "undefined") & (busRoute !== "")) {
-    let busRouteURL = "https://api.umd.io/v0/bus/routes"; // Your code here
+    let busRouteURL = "https://api.umd.io/v0/bus/routes/" + busRoute; // Your code here
     console.log(busRouteURL);
-    const fetch = fetch(busURL);
+
 
     fetch(busRouteURL)
       .then((response) => {
         return response.json();
       })
       .then((route) => {
-        // YOUR CODE HERE
+        // YOUR CODE HERE 
         console.log("Here");
         console.log(route);
-        let title, latMax, latMin, lonMax, lonMin;
+        let title, lat_max, lat_min, lon_max, lon_min;
         let message = "";
 
-        if(typeof route.title != "undefined"){
-          title = title.route;
-          latMax = route.lat_max;
-          latMin = route.lat_min;
-          lonMax = route.lon_max;
-          lonMin = route.lon_min;
 
-          message = title + " <br> Lat max : " + LatMax + " <br> Lat min: " + LatMin + " <br> Lon max " + lonMax + " <br>Lon min" + lonMin;
-        }
-        else{
-          message = "No bus info available";
-        }
+        title = route.title;
+        lat_max = route.lat_max;
+        lat_min = route.lat_min;
+        lon_max = route.lon_max;
+        lon_min = route.lon_min;
+        sessionStorage.setItem("title", title);
+        sessionStorage.setItem("lat_max", lat_max);
+        sessionStorage.setItem("lat_min", lat_min);
+        sessionStorage.setItem("lon_max", lon_max);
+        sessionStorage.setItem("lon_min", lon_min);
+        message = "Info saved"
+
+
       })
       .catch((err) => {
         console.log(err);
@@ -38,5 +40,10 @@ function getBusRoute() {
       });
   } else {
     main.innerHTML = "No value provided";
+    sessionStorage.setItem("title", "nothing set");
+    sessionStorage.setItem("lat_max", "nothing set");
+    sessionStorage.setItem("lat_min", "nothing set");
+    sessionStorage.setItem("lon_max", "nothing set");
+    sessionStorage.setItem("lon_min", "nothing set");
   }
 }
